f07274ff878ad40176e32efbe75e389a09e6f568
trananhminh <trananhminh910@gmail.com>
2021-11-14 15:24:50 +0700

    chapter 7: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = "/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i".freeze
   before_save :downcase_email
   has_secure_password
   has_secure_password :recovery_password, validations: false
@@ -10,16 +9,14 @@ class User < ApplicationRecord
 
   validates :account_name,
             presence: true,
-            length: {in: 6..20, message: "Enter 6 - 12 character, please"}
+            uniqueness: true,
+            length: {in: 6..20}
 
   validates :email,
             presence: true,
             uniqueness: true,
-            length: {
-              maximum: 150,
-              too_long: "%{count} character maximum allowed"
-            },
-            format: {with: VALID_EMAIL_REGEX}
+            length: {maximum: 150},
+            format: {with: URI::MailTo::EMAIL_REGEXP}
 
   validates :age,
             presence: true,
@@ -27,10 +24,7 @@ class User < ApplicationRecord
 
   validates :gender,
             presence: true,
-            inclusion: {
-              in: %w(male female other),
-              message: "%{value} is not gender"
-            }
+            inclusion: {in: %w(male female other)}
 
   def downcase_email
     email.downcase!
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
     root "static_pages#home"
     get "/home", to: "static_pages#home"
     get "/help", to: "static_pages#help"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users
   end
 end
