fd45916c4beceb2f3f33236bd9f2b2f5bc172d09
trananhminh <trananhminh910@gmail.com>
2021-11-16 10:01:53 +0700

    chapter 8: Basic Login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by account_name: params[:session][:account_name].downcase
+    if user&.authenticate params[:session][:password]
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = t "invalid_account_password_combination"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
